14788ecab64c91a9d0c5c23929a5e63156e9596d
Lucas DeBruyn <lucas.debruyn@mths.ca>
2022-12-08 23:35:42 +0000

    done

---
+++ rangeloop.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Lucas DeBruyn
+# Created on: Oct 2021
+# This program lists every number from 1000 to 2000
+
+
+def main():
+    # This function lists every number from 1000 to 2000
+
+    counter = 0
+
+    # Process and Output
+    for counter in range(1000, 2001):
+        if counter % 5 == 0:
+            print("")
+            print(counter, end=" ")
+        else:
+            print("{0} ".format(counter), end="")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
